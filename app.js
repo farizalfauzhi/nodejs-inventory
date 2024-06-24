58f209a124f9635d7516fbef72cd20230a8fa0d5
gita gustia <farizalfauzi69@gmail.com>
2024-06-24 20:28:40 +0700

    bikin aplikasi dan route awal

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+
+app.use( express.urlencoded({extended:false}) )
+app.set('view engine', 'ejs')
+app.set('views', './view-html')
+
+app.get('/', c_beranda.index)
+
+app.listen(port, ()=>{
+    console.log(`App sudah siap, buka http://localhost:${port}`)
+})
